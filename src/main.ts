524ece2f0c2c9eff8b94f8f139f75cfc560c58b2
jesus  henriquez mura <jesus.henriquez.m@usach.cl>
2021-11-18 19:38:21 -0300

    add validation data

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 //levanta el servidor
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { ValidationPipe } from "@nestjs/common";
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
 bootstrap();
--- src/task/dto/task.dto.ts
+++ src/task/dto/task.dto.ts
@@ -1,4 +1,11 @@
+import { IsBoolean, IsNotEmpty, IsString, MaxLength, MinLength } from "class-validator";
+
 export class TaskDTO {
+  @IsNotEmpty()
+  @IsString()
+  @MaxLength(100)
   readonly description: string;
+  @IsNotEmpty()
+  @IsBoolean()
   readonly isDone: boolean;
 }
