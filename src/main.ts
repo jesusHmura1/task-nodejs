84de5c00539cd8a2b5a6fe3aa21e14e7b142e6f3
jesus  henriquez mura <jesus.henriquez.m@usach.cl>
2021-11-18 17:30:59 -0300

    add initial project

---
+++ src/main.ts
@@ -0,0 +1,9 @@
+//levanta el servidor
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(3000);
+}
+bootstrap();
