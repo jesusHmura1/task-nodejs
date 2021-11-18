4c8d8bdeaf88f864e940a3a206e32438899221d2
jesus  henriquez mura <jesus.henriquez.m@usach.cl>
2021-11-18 19:30:24 -0300

    add CRUD task

--- src/task/task.module.ts
+++ src/task/task.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { TaskController } from './task.controller';
+import { TaskService } from './task.service';
 
 @Module({
   controllers: [TaskController],
-  providers: [],
+  providers: [TaskService],
 })
 export class TaskModule {}
